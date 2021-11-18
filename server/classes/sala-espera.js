b97add93ebe12274bf4ac148c6439ea0f88144d0
SebastianC8 <scorrales@garantiascomunitarias.com>
2021-11-18 11:29:19 -0500

    Avance sockets: sala de espera

--- server/classes/sala-espera.js
+++ server/classes/sala-espera.js
@@ -1,23 +1,43 @@
 class SalaEspera
 {
+    /**
+     * Array para añadir usuarios
+     */
     listaSalaEspera = [];
 
+    /**
+     * Función encargada de añadir un usuario a la sala cuando se le envíe un SMS
+     * @param {} data 
+     * @param {*} socketId 
+     */
     addUserRoom = (data, socketId) => {
         
-        const user = this.listaSalaEspera.find((row) => row.id_convocado_reunion == data.idConvocadoReunion);
+        /**Se busca el usuario en la lista */
+        const user = this.getUserList(data.idConvocadoReunion);
 
         if (!user) {
+            /** Si no existe, se añade al array */
             this.listaSalaEspera.push({ socketId, id_convocado_reunion: data.idConvocadoReunion });
         } else {
+            /** Si existe, se modifica el id del socket */
             user.socketId = socketId;
         }
 
     }
 
+    /**
+     * Función encargada de consultar un usuario en la lista de usuarios
+     * @param {*} idConvocadoReunion 
+     * @returns  array
+     */
     getUserList = (idConvocadoReunion) => {
         return this.listaSalaEspera.find((row) => row.id_convocado_reunion == idConvocadoReunion);
     }
 
+    /**
+     * Función encargada de consultar todos los usuarios que están en la sala de espera
+     * @returns array
+     */
     getListaUsuarios = () => this.listaSalaEspera;
 
 }
--- server/sockets/socket-sala-espera.js
+++ server/sockets/socket-sala-espera.js
@@ -16,20 +16,31 @@ io.on('connection', (socket) => {
      */
     socket.join(room);
 
+    /**
+     * Escucha socket cuando se envía SMS para proceso de firma
+     */
     socket.on('registro-representante', (data) => {
+        /** Se añade usuario a la sala */
         salaEspera.addUserRoom(data, socket.id);
     });
 
+    /**
+     * Recibe url_firma para emitirla al frontend
+     */
     app.get('/get-url-firma', (req, res) => {
 
+        /** Valida parámetros requeridos */
         if ('url_firma' in req.query && 'id_convocado_reunion' in req.query) {
             
+            /** Busca al usuario en la sala */
             const user = salaEspera.getUserList(req.query.id_convocado_reunion);
 
             if (user) {
+                /** Emite socket al respectivo usuario y le envía la url de la firma */
                 io.to(user.socketId).emit('fin-firma', { url: req.query.url_firma });
             }
 
+            /** Respuesta para laravel */
             res.send({
                 'ok' : (user) ? true : false,
                 'response' : (user) ? user : 'No se encontró usuario'
