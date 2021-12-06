6f77f7f88edaaddc485de26ab5f75e8341a1d307
SebastianC8 <scorrales@garantiascomunitarias.com>
2021-12-06 18:06:32 -0500

    avances sockets

--- server/classes/reunion.js
+++ server/classes/reunion.js
@@ -1,8 +1,10 @@
+const axios = require('axios');
+
 class Reunion
 {
-
     listaConvocados = [];
     listaAdministradores = [];
+    urlAPI = 'http://192.168.2.85:8800/api';
 
     agregarLista = (tipo, data) => {
         if (this[tipo]) {
@@ -15,7 +17,21 @@ class Reunion
         }
     }
 
+    sacarUsuario = (socket_id) => {
+        this.listaConvocados = this.listaConvocados.filter((row) => row.socketId != socket_id);
+        this.listaAdministradores = this.listaAdministradores.filter((row) => row.socketId != socket_id);
+    }
+
     getLista = (tipo) => this[tipo];
+
+    storeUser = (data) => {
+        axios.post(`${this.urlAPI}/acceso-reunion/guardar-acceso-reunion`, data)
+        .then((response) => {
+            if (response.status === 200) {
+                console.log(response.data);
+            }
+        }).catch((error) => console.log('error', error));
+    }
 
 }
 
--- server/sockets/socket-reunion.js
+++ server/sockets/socket-reunion.js
@@ -3,11 +3,21 @@ const { Reunion } = require('../classes/reunion');
 
 const reunion = new Reunion();
 
+const room = 'reunion-gc';
+
 io.on('connection', (socket) => {
+
+    socket.room = room;
+
+    socket.join(room);
 
     socket.on('acceso-reunion', (data) => {
         const tipoAcceso = (data.tipo_convocado === 'Convocado') ? 'listaConvocados' : 'listaAdministradores';
         reunion.agregarLista(tipoAcceso, { ...data, socketId: socket.id });
+        reunion.storeUser(data);
+        io.in(room).emit('estadoUsuario', { usuario: data });
     });
+
+    socket.on('disconnect', () => reunion.sacarUsuario(socket.id));
 
 });
