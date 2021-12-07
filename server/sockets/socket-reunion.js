6352235e4a286accd5987b79472e2c8c07cd935e
SebastianC8 <scorrales@garantiascomunitarias.com>
2021-12-07 17:37:26 -0500

    avances sockets

--- server/sockets/socket-reunion.js
+++ server/sockets/socket-reunion.js
@@ -12,10 +12,15 @@ io.on('connection', (socket) => {
     socket.join(room);
 
     socket.on('acceso-reunion', (data) => {
+
         const tipoAcceso = (data.tipo_convocado === 'Convocado') ? 'listaConvocados' : 'listaAdministradores';
+
         reunion.agregarLista(tipoAcceso, { ...data, socketId: socket.id });
         reunion.storeUser(data);
+
         io.in(room).emit('estadoUsuario', { usuario: data });
+        io.in(room).emit('listaUsuarios', reunion.getLista(tipoAcceso).map((row) => row.identificacion).filter((row) => row !== data.identificacion));
+        
     });
 
     socket.on('disconnect', () => reunion.sacarUsuario(socket.id));
